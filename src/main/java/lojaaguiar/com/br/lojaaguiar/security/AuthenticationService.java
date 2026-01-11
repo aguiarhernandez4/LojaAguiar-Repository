b861be14cdb75a96cce3104ce1da052b97b80b6f
João Pedro <aguiarhernandez4@gmail.com>
2026-01-11 12:47:32 -0300

    Adicionado o Authentication service

--- src/main/java/lojaaguiar/com/br/lojaaguiar/security/AuthenticationService.java
+++ src/main/java/lojaaguiar/com/br/lojaaguiar/security/AuthenticationService.java
@@ -1,12 +1,16 @@
 package lojaaguiar.com.br.lojaaguiar.security;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.core.Authentication;
 
 @Service
 public class AuthenticationService {
 
     @Autowired
     JwtService jwtService;
+
+    public String authenticate(Authentication authentication) {
+        return jwtService.generateToken(authentication);
+    }
 }
