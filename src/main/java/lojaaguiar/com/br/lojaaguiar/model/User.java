393e3ad39044e946018fe97ac7c7909e3ae94460
João Pedro <aguiarhernandez4@gmail.com>
2026-01-11 11:54:37 -0300

    Criação da entidade usuario básica

--- src/main/java/lojaaguiar/com/br/lojaaguiar/model/User.java
+++ src/main/java/lojaaguiar/com/br/lojaaguiar/model/User.java
@@ -1,10 +1,37 @@
 package lojaaguiar.com.br.lojaaguiar.model;
 
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "USERS")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_USER")
+    private Long idUser;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "PASSWORD")
+    private  String password;
+
+
 }
