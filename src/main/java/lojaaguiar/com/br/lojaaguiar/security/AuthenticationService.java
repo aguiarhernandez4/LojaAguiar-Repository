c592016d05b5c4d3a18f560ea2169615c554ffee
João Pedro <aguiarhernandez4@gmail.com>
2026-01-11 12:45:16 -0300

    Adicionando JwtService

--- src/main/java/lojaaguiar/com/br/lojaaguiar/security/AuthenticationService.java
+++ src/main/java/lojaaguiar/com/br/lojaaguiar/security/AuthenticationService.java
@@ -1,4 +1,12 @@
 package lojaaguiar.com.br.lojaaguiar.security;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthenticationService {
+
+    @Autowired
+    JwtService jwtService;
 }
