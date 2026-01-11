5388114fe64a4d056d5d837efbb3af36440fea64
João Pedro <aguiarhernandez4@gmail.com>
2026-01-11 11:36:13 -0300

    Criando branch main

---
+++ src/main/java/lojaaguiar/com/br/lojaaguiar/LojaAguiarApplication.java
@@ -0,0 +1,13 @@
+package lojaaguiar.com.br.lojaaguiar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaAguiarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LojaAguiarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lojaaguiar/com/br/lojaaguiar/LojaAguiarApplicationTests.java
@@ -0,0 +1,13 @@
+package lojaaguiar.com.br.lojaaguiar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaAguiarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
