7c09b2adead4751dff01bc6ce6cff0f82a1f4d6d
João Pedro <aguiarhernandez4@gmail.com>
2026-01-18 18:04:33 -0300

    Adicionado @Nonnull

--- src/main/java/lojaaguiar/com/br/lojaaguiar/security/UserDetailsServiceImpl.java
+++ src/main/java/lojaaguiar/com/br/lojaaguiar/security/UserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package lojaaguiar.com.br.lojaaguiar.security;
 
+import jakarta.annotation.Nonnull;
 import lojaaguiar.com.br.lojaaguiar.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,7 +15,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    @Nonnull
+    public UserDetails loadUserByUsername(@Nonnull String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username)
                 .map(user -> new UserAuthenticated(user))
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
